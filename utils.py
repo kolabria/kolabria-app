a917b154fc6bc4813a44318ae739679c7075d36c
Alok Mohindra <alok.mohindra@gmail.com>
2012-04-25 02:58:56 -0400

    removed slugify unicode -- no turkish required

--- utils.py
+++ utils.py
@@ -1,8 +1,4 @@
 # -*- coding: utf-8 -*-
-
-import unicodedata, re
-from django.utils.safestring import mark_safe
-
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
@@ -10,30 +6,17 @@ from django.views.generic import View
 from django.core.urlresolvers import reverse
 
 
-def slugify_unicode(value):
-    """
-        Türkçe karakterleri yutmayan slugify fonksiyonu.
-        http://gokmengorgen.net/post/detail/djangoda-turkce-destekli-slugify/
-    """
-    
-    value = value.replace(u'\u0131', 'i')
-    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
-    value = unicode(re.sub('[^\w\s-]', '', value).strip().lower())
-    
-    return mark_safe(re.sub('[-\s]+', '-', value))
-
-
 class ProtectedView(View):
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super(ProtectedView, self).dispatch(*args, **kwargs)
-    
-    
+
+
 class AuthorProtectedView(ProtectedView):
     def dispatch(self, *args, **kwargs):
         post = self.get_queryset()
-        
+
         if not post.author != self.request.user:
             return redirect('access_denied')
-        
+
         return super(AuthorProtectedView, self).dispatch(*args, **kwargs)
